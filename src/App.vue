e0e0af9e44242b2493b38961388aabf1b8d7b7e6
zhangce <z874010534@vip.qq.com>
2023-03-16 15:56:50 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="cesiumContainer">
+  </div>
+</template>
+
+<script setup>
+import * as cesium from "cesium";
+import {onMounted} from "vue";
+
+onMounted(() => {
+  const custom = new cesium.ArcGisMapServerImageryProvider({
+    url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+  })
+  const viewer = new cesium.Viewer('cesiumContainer', {
+    // baseLayerPicker: false, // 右上角图块
+    imageryProvider: custom, // 可视化图像数据
+  })
+
+})
+</script>
+<style scoped>
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
