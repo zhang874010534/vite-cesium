d361af73f0f7a06062fdddfab3c38577bd532602
zhangce <z874010534@vip.qq.com>
2023-03-16 18:19:11 +0800

    feat: cesium 摄像机控制视角朝向

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,23 @@ onMounted(() => {
     url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
   })
   const viewer = new cesium.Viewer('cesiumContainer', {
-    // baseLayerPicker: false, // 右上角图块
+    baseLayerPicker: false, // 右上角图块
     imageryProvider: custom, // 可视化图像数据
+    terrainProvider:  cesium.createWorldTerrain({
+      requestWaterMask: true, // 水的效果
+      requestVertexNormals: true
+    }), // 地形
   })
-
+  // 摄像机 就是一开始的地方设置
+  viewer.camera.setView({
+    destination: cesium.Cartesian3.fromDegrees(113.318977, 23.114155, 200), // 经纬度高度
+    // 观看角度
+    orientation: {
+      // heading: 1,
+      heading: cesium.Math.toRadians(0),
+      pitch: cesium.Math.toRadians(-90.0),
+    }
+  })
 })
 </script>
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,19 @@ export default defineConfig({
     vue(),
     cesium()
   ],
+  server: {
+    open: false,
+    host: 'localhost',
+    port: '8080',
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '/api': {
+        target: 'https://services.arcgisonline.com',
+        secure: true,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    }
+  }
 })
